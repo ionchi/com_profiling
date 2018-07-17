55379331af09f3c6b4e5000c5e76bd9c120b163a
ionchi <ion.chiriac@live.com>
2018-07-17 20:44:28 +0200

    fix

--- Semantico800.py
+++ Semantico800.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 import os
 from pyspark.sql import SparkSession
 
-INPUT_CSV="InteressiUtentiRank.csv"
+INPUT_CSV = "InteressiUtentiRank.csv"
 
 if __name__ == "__main__":
     spark = SparkSession \
@@ -18,27 +18,27 @@ if __name__ == "__main__":
     # remove whitespace characters like `\n` at the end of each line
     clique_ids = [x.strip() for x in clique_ids]
 
-    collezione = df_communities.select('nodes', '_id.oid','com_id','cliques').collect()
-    cliqueUtenti={}
-    CliqueInComm={}
-    for i in collezione:
-        if(i.com_id!=None):
-            cliqueUtenti[i.oid]=i.nodes
-            CliqueInComm[i.oid]=len(i.cliques)
+    collection = df_communities.select('nodes', '_id.oid', 'com_id', 'cliques').collect()
+    cliqueUtenti = {}
+    CliqueInComm = {}
+    for i in collection:
+        if i.com_id is not None:
+            cliqueUtenti[i.oid] = i.nodes
+            CliqueInComm[i.oid] = len(i.cliques)
 
-    UtenteInteressi={}
+    UtenteInteressi = {}
     for i in clique_ids:
         try:
-            i=i.split(",")
-            utente=i[0]
-            interesse=i[1]
-            score=i[2]
-            if(utente!='' and utente!='Utente'):
-                a=tuple([interesse,int(score)])
+            i = i.split(",")
+            utente = i[0]
+            interesse = i[1]
+            score = i[2]
+            if utente != '' and utente != 'Utente':
+                a = tuple([interesse, int(score)])
                 if utente not in UtenteInteressi:
-                    lista=[]
+                    lista = []
                     lista.append(a)
-                    UtenteInteressi[utente]=lista
+                    UtenteInteressi[utente] = lista
                 else:
                     UtenteInteressi[utente].append(a)
         except:
@@ -54,32 +54,32 @@ if __name__ == "__main__":
     dataSet.write("com_id, Nodi, MassimaFreq, Conteggio, NumeroClique, intrusi\n")
     print("Created empty file: " + OUTPUT_FILE)
 
-    InteressiFin={}
+    InteressiFin = {}
     for cliq in cliqueUtenti:
-        mappaApp={}
-        intrusi=0
-        lista=[]
+        mappaApp = {}
+        intrusi = 0
+        lista = []
         for utente in cliqueUtenti[cliq]:
             try:
                 for riga in UtenteInteressi[utente]:
                     if riga[0] in mappaApp:
-                        if riga[1]>=4:                        #mappaApp[riga[0]] =mappaApp[riga[1]]
-                            mappaApp[riga[0]]+=1
+                        if riga[1] >= 4:                        # mappaApp[riga[0]] = mappaApp[riga[1]]
+                            mappaApp[riga[0]] += 1
                     else:
-                        if riga[1]>=4:                        #mappaApp[riga[0]] =mappaApp[riga[1]]
-                            mappaApp[riga[0]]=1
+                        if riga[1] >= 4:                        # mappaApp[riga[0]] = mappaApp[riga[1]]
+                            mappaApp[riga[0]] = 1
             except:
                 lista.append(utente)
-                intrusi+=1
-
+                intrusi += 1
 
         InteressiFin[cliq] = mappaApp
-        massimo=max(mappaApp.values())
-        conteggio=0
+        massimo = max(mappaApp.values())
+        conteggio = 0
         for i in mappaApp:
-            if mappaApp[i]==massimo:
-                conteggio+=1
-        dataSet.write(str(cliq)+","+str(len(cliqueUtenti[cliq]))+","+str(massimo)+","+str(conteggio)+","+str(CliqueInComm[cliq])+","+str(intrusi)+"\n")
+            if mappaApp[i] == massimo:
+                conteggio += 1
+        dataSet.write(str(cliq) + "," + str(len(cliqueUtenti[cliq])) + "," + str(massimo)+"," + str(conteggio)
+                      + "," + str(CliqueInComm[cliq]) + "," + str(intrusi)+"\n")
 
 
 
