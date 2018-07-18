48712f4913433207a3820ad0307ece1101d75dfc
ionchi <ion.chiriac@live.com>
2018-07-18 20:59:32 +0200

    fix

--- Output.py
+++ Output.py
@@ -5,14 +5,14 @@ import os
 
 from pyspark.sql import SparkSession
 
-media= 0.9235718909805637
+media = 0.9235718909805637
 deviazione = 0.08820130230264378
-media_scartate= 0.7246243159329131
-normal_1=0.1298673421199502
-normal_2=0.02326879319967372
+media_scartate = 0.7246243159329131
+normal_1 = 0.1298673421199502
+normal_2 = 0.02326879319967372
 
-INPUT_CSV="InteressiUtentiRank.csv"
-INPUT2='clique_topologica.csv'
+INPUT_CSV = "InteressiUtentiRank.csv"
+INPUT2 = 'clique_topologica.csv'
 if __name__ == "__main__":
     spark = SparkSession \
         .builder \
@@ -23,50 +23,46 @@ if __name__ == "__main__":
     # # contiene tutta la collection cliqueInput
     df_communities = spark.read.format("com.mongodb.spark.sql").load()
 
-
-
     with open(INPUT_CSV) as f:
         clique_ids = f.readlines()
     # remove whitespace characters like `\n` at the end of each line
     clique_ids = [x.strip() for x in clique_ids]
 
-    f2=open(INPUT2,'r')
-    amicizie=[]
-    reader=csv.reader(f2)
+    f2 = open(INPUT2, 'r')
+    amicizie = []
+    reader = csv.reader(f2)
     for row in reader:
         amicizie.append(row)
     for i in range(len(amicizie)):
         amicizie[i][1] = float(amicizie[i][1])
 
-
-    #estraggo per ogni clique l'insieme degli utenti
+    # estraggo per ogni clique l'insieme degli utenti
     collezione = df_communities.select('nodes', '_id.oid').collect()
-    cliqueUtenti={}
+    cliqueUtenti = {}
     for i in collezione:
-        cliqueUtenti[i.oid]=i.nodes
+        cliqueUtenti[i.oid] = i.nodes
 
-    UtenteInteressi={}
+    UtenteInteressi = {}
     for i in clique_ids:
         try:
-            i=i.split(",")
-            utente=i[0]
-            interesse=i[1]
-            score=i[2]
-            if(utente!='' and utente!='Utente'):
-                a=tuple([interesse,int(score)])
+            i = i.split(",")
+            utente = i[0]
+            interesse = i[1]
+            score = i[2]
+            if (utente != '' and utente != 'Utente'):
+                a = tuple([interesse, int(score)])
                 if utente not in UtenteInteressi:
-                    lista=[]
+                    lista = []
                     lista.append(a)
-                    UtenteInteressi[utente]=lista
+                    UtenteInteressi[utente] = lista
                 else:
                     UtenteInteressi[utente].append(a)
         except:
             pass
 
+    interessiComm = {}
 
-    interessiComm={}
-
-    OUTPUT_FILE="OutputFinale.csv"
+    OUTPUT_FILE = "OutputFinale.csv"
 
     if os.path.exists(OUTPUT_FILE):
         os.remove(OUTPUT_FILE)
@@ -77,68 +73,61 @@ if __name__ == "__main__":
     print("Created empty file: " + OUTPUT_FILE)
 
     for cliq in cliqueUtenti:
-        mappaApp={}
-        intrusi=0
-        lista=[]
+        mappaApp = {}
+        intrusi = 0
+        lista = []
         for utente in cliqueUtenti[cliq]:
             try:
                 for riga in UtenteInteressi[utente]:
                     if riga[0] in mappaApp:
-                        if riga[1]>=4:                        #mappaApp[riga[0]] =mappaApp[riga[1]]
-                            mappaApp[riga[0]]+=1
+                        if riga[1] >= 4:  # mappaApp[riga[0]] =mappaApp[riga[1]]
+                            mappaApp[riga[0]] += 1
                     else:
-                        if riga[1]>=4:                        #mappaApp[riga[0]] =mappaApp[riga[1]]
-                            mappaApp[riga[0]]=1
+                        if riga[1] >= 4:  # mappaApp[riga[0]] =mappaApp[riga[1]]
+                            mappaApp[riga[0]] = 1
             except:
                 lista.append(utente)
-                intrusi+=1
+                intrusi += 1
 
-
-
-        #verifica se è community o meno
-
+        # verifica se è community o meno
 
         interessiComm[cliq] = mappaApp
-        massimo=max(mappaApp.values())
-        conteggio=0
-        appLista={}
+        massimo = max(mappaApp.values())
+        conteggio = 0
+        appLista = {}
         for i in mappaApp:
-            if mappaApp[i]==massimo:
-                appLista[cliq]=i
-                conteggio+=1
-        freq=massimo/len(cliqueUtenti[cliq])
-        if freq>media:
-            semantico=1
+            if mappaApp[i] == massimo:
+                appLista[cliq] = i
+                conteggio += 1
+        freq = massimo / len(cliqueUtenti[cliq])
+        if freq > media:
+            semantico = 1
         else:
-            if freq>(media-deviazione):
-                semantico=freq
+            if freq > (media - deviazione):
+                semantico = freq
             else:
-                if freq >media_scartate:
-                    semantico=freq*normal_1
+                if freq > media_scartate:
+                    semantico = freq * normal_1
                 else:
-                    semantico=freq*normal_2
+                    semantico = freq * normal_2
+        mancanti = 0
         for cliq2 in amicizie:
-            if cliq2[0]==cliq:
-                mancanti=cliq2[1]
-        if mancanti==0:
-            topologico=1
+            if cliq2[0] == cliq:
+                mancanti = cliq2[1]
+        if mancanti == 0:
+            topologico = 1
         else:
-            if mancanti==1:
-                topologico=0.031
+            if mancanti == 1:
+                topologico = 0.031
             else:
-                topologico=0
-        community=semantico*70+topologico*30
-        if community>=60:
-            scelta="si"
+                topologico = 0
+        community = semantico * 70 + topologico * 30
+        if community >= 60:
+            scelta = "si"
         else:
-            scelta="no"
+            scelta = "no"
 
-        dataSet.write(str(cliq)+","+str(scelta)+","+str(community)+"\n")
-
-
-
-
+        dataSet.write(str(cliq) + "," + str(scelta) + "," + str(community) + "\n")
 
     dataSet.close()
     spark.stop()
-
--- finalScript.py
+++ finalScript.py
@@ -2,23 +2,29 @@ from pymongo import MongoClient
 
 import os
 
+MEDIA = 0.9235718909805637
+DEVIAZIONE = 0.08820130230264378
+MEDIA_SCARTATE = 0.7246243159329131
+NORMAL_1 = 0.1298673421199502
+NORMAL_2 = 0.02326879319967372
+
 MONGO_HOST = 'localhost'
 MONGO_PORT = 27017
 
-DATABASE = 'twitter'
-OUTPUT_FILE = 'results.csv'
+DATABASE = 'bigdata_db'
+OUTPUT_FILE = 'helpers/results.csv'
 
 if __name__ == "__main__":
     if os.path.exists(OUTPUT_FILE):
         os.remove(OUTPUT_FILE)
         print("Removed: " + OUTPUT_FILE)
     with open(OUTPUT_FILE, "w") as output:
-        output.write("clique_id,numero_nodi,amicizie_mancanti,simmetrie_mancanti,massimo_score_interessi,count_score_massimi \n")
+        output.write("clique_id, Community, percentuale si \n")
 
     client = MongoClient(MONGO_HOST, MONGO_PORT)
     db = client[DATABASE]
 
-    collectionClique = db.clique_input.find({})
+    collectionClique = db.clique_dataset.find({})
     count = 0
     userInterests = {}
     for clique in collectionClique:
@@ -33,9 +39,9 @@ if __name__ == "__main__":
         for user_id in clique['nodes']:
             # analisi semantica
             try:
-                user_interests = db.user_infos.find({'user': user_id})[0]['info']['interests']['all']
+                user_interests = db.user_info.find({'user': user_id})[0]['info']['interests']['all']
                 for interest in user_interests:
-                    interest_score = db.user_infos.find({'user': user_id})[0]['info']['interests']['all'][interest]['score']
+                    interest_score = db.user_info.find({'user': user_id})[0]['info']['interests']['all'][interest]['score']
                     a = tuple([interest, int(interest_score)])
                     if user_id not in userInterests:
                         interest_tuple = [a]
@@ -57,12 +63,12 @@ if __name__ == "__main__":
                 userSenzaInteressiPerClique += 1
 
             # analisi topologica
-            user_friends = db.twitternetworks.find({'id': user_id})[0]['friends']   # indice 0 perché è solo una la lista di amici per utente
+            user_friends = db.twitter_networks.find({'id': user_id})[0]['friends']   # indice 0 perché è solo una la lista di amici per utente
 
             for friend_id in clique['nodes']:
                 if friend_id != user_id:
                     if friend_id not in user_friends:
-                        other_user_friends = db.twitternetworks.find({'id': friend_id})[0]['friends']
+                        other_user_friends = db.twitter_networks.find({'id': friend_id})[0]['friends']
                         if user_id not in other_user_friends:
                             not_friends += 1
                         else:
@@ -74,7 +80,32 @@ if __name__ == "__main__":
             if mappaApp[interest] == maxScoreInClique:
                 countMaxScores += 1
 
+        freq = maxScoreInClique / len(clique['nodes'])
+        if freq > MEDIA:
+            semantico = 1
+        else:
+            if freq > (MEDIA - DEVIAZIONE):
+                semantico = freq
+            else:
+                if freq > MEDIA_SCARTATE:
+                    semantico = freq * NORMAL_1
+                else:
+                    semantico = freq * NORMAL_2
+
+        if not_friends == 0:
+            topologico = 1
+        else:
+            if not_friends == 1:
+                topologico = 0.031
+            else:
+                topologico = 0
+
+        community = semantico * 70 + topologico * 30
+        if community >= 60:
+            scelta = "si"
+        else:
+            scelta = "no"
+
+        print(str(clique['_id']) + "," + str(scelta) + "," + str(community))
         with open(OUTPUT_FILE, "a") as output:
-            output.write(str(clique['_id']) + "," + str(len(clique['nodes'])) + ","
-                         + str(not_friends) + "," + str(noSimm) + ","
-                         + str(maxScoreInClique) + "," + str(countMaxScores) + "\n")
+            output.write(str(clique['_id']) + "," + str(scelta) + "," + str(community) + "\n")
